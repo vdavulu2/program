53df7c0b3d3ed366f902e24b037d998120f5737b
vdavulu2 <krishnadavuluri123@gmail.com>
2024-01-03 15:16:38 -0600

    Create odd or even number program.py

---
+++ odd or even number program.py
@@ -0,0 +1,8 @@
+
+
+number = float(input("Enter any number to test whether it is odd or even: "))
+if (number % 2) == 0:
+    print("The number is even")
+else:
+    print("The provided number is odd")
+
